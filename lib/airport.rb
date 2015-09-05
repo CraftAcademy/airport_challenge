15dba2a827c509b0afc24ac61b338d984ee2c753
Christopher Comella <chris@Christophers-MacBook-Pro.local>
2015-09-05 14:27:02 +0200

    create first passing airport tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,35 @@
+require 'plane.rb'
+
 class Airport
+
+  attr_accessor :hanger
+
+  def initialize(hanger_capacity)
+    @hanger_capacity = hanger_capacity
+
+    @hanger = Array.new()
+
+    while @hanger.length < hanger_capacity
+      @hanger << Plane.new
+    end
+
+    @hanger
+  end
+
+  def take_off
+
+  end
+
+  def land
+
+  end
+
+  def traffic_control
+
+  end
+
+  def weather_conditions
+
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,7 @@
 require 'airport'
 
-## Note these are just some guidelines!
-## Feel free to write more tests!!
+# Note these are just some guidelines!
+# Feel free to write more tests!!
 
 # A plane currently in the airport can be requested to take off.
 #
@@ -13,13 +13,25 @@ require 'airport'
 
 describe Airport do
 
+  let(:airport) { Airport.new(3) }
+  let(:plane) { airport.hanger.pop }
+
+  it "creates a new airport with a capacity of three planes" do
+    expect(airport).to be_a_kind_of(Airport)
+  end
+
+  it "the airport's hanger contains three planes" do
+    expect(airport.hanger.length).to eq(3)
+    expect(plane).to be_a_kind_of(Plane)
+  end
+
   describe 'take off' do
     xit 'instructs a plane to take off'
 
     xit 'releases a plane'
   end
 
-  describe 'landing' do
+  describe 'land' do
     xit 'instructs a plane to land'
 
     xit 'receives a plane'
